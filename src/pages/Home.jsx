372040ccf096eb24d4d92d0d312e5c324f1dbf89
fetchthesauce <rukjaa908@gmail.com>
2026-08-26 04:42:03 +0530

    add basic home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Home</h1>
+      {/* Video Grid will go here */}
+    </div>
+  );
+};
+
+export default Home;
