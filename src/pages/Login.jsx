f1171bdf1c812ac399518b4efcdaab4d9e775f6b
fetchthesauce <rukjaa908@gmail.com>
2026-08-26 21:42:03 +0530

    add login form ui

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center h-[calc(100vh-64px)]">
+      <form className="bg-white p-8 rounded shadow-md w-96">
+        <h2 className="text-2xl mb-4 font-semibold text-center">Sign In</h2>
+        {/* Form fields will go here */}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
