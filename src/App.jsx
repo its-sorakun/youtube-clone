233d37a168a420bede42558b3349a7bc5ba6228a
fetchthesauce <rukjaa908@gmail.com>
2026-08-28 08:42:03 +0530

    setup main layout with outlet

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import Sidebar from './components/Sidebar.jsx';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col font-sans">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 md:ml-64 bg-gray-50 p-4">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
