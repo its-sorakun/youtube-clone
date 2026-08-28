b1e4c0383a674aff97d6a2086c03f082fe81e53d
fetchthesauce <rukjaa908@gmail.com>
2026-08-28 17:42:03 +0530

    configure browser router

---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import VideoPlayer from './pages/VideoPlayer.jsx';
+import Channel from './pages/Channel.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/video/:id',
+        element: <VideoPlayer />
+      },
+      {
+        path: '/channel/:id',
+        element: <Channel />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      }
+    ]
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
