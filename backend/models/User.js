e44f705f71b03e0f9e54284990122d1529621a76
fetchthesauce <rukjaa908@gmail.com>
2026-08-29 16:42:03 +0530

    add user schema

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    default: 'https://via.placeholder.com/150'
+  },
+  channels: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Channel'
+  }]
+}, { timestamps: true });
+
+export default mongoose.model('User', userSchema);
