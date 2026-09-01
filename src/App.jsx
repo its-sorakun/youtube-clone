c444f27414acf59ba9732c2e8efac8bf08245cbe
fetchthesauce <rukjaa908@gmail.com>
2026-09-01 19:18:55 +0530

    wrap app with auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Sidebar from './components/Sidebar.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col font-sans">
-      <Header />
-      <div className="flex flex-1">
-        <Sidebar />
-        <main className="flex-1 md:ml-64 bg-gray-50 p-4">
-          <Outlet />
-        </main>
+    <AuthProvider>
+      <div className="min-h-screen bg-gray-50 flex flex-col font-sans">
+        <Header />
+        <div className="flex flex-1">
+          <Sidebar />
+          <main className="flex-1 md:ml-64 bg-gray-50 p-4">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   );
 }
 
